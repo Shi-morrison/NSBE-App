ab69b28f1e58730e84326e23122d4a817f092af6
Shi-morrison <shian.morrison@gmail.com>
2023-12-14 15:34:13 -0500

    Changed project to use typescript

--- App/babel.config.js
+++ App/babel.config.js
@@ -1,19 +1,6 @@
-module.exports = api => {
+module.exports = function(api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo', 'module:metro-react-native-babel-preset'],
-    plugins: [
-      [
-        'module:react-native-dotenv',
-        {
-          moduleName: '@env',
-          path: '.env',
-          blacklist: null,
-          whitelist: null,
-          safe: false,
-          allowUndefined: false
-        }
-      ]
-    ]
+    presets: ['babel-preset-expo'],
   };
 };
