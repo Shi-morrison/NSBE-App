4f3d5ebb3106c74f529996d429bf8d7495b3518a
Shi-morrison <shian.morrison@gmail.com>
2023-12-14 17:18:53 -0500

    tailwind attempt lol

--- App/App.tsx
+++ App/App.tsx
@@ -1,19 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import {TailwindProvider} from 'tailwind-rn';
+import { TailwindProvider } from 'tailwind-rn';
+import { useTailwind } from 'tailwind-rn';
 import utilities from './tailwind.json';
 import Calendar from './components/Calendar'
 
+
 export default function App() {
+  const tailwind = useTailwind();
   return (
-      <>
+    <>
+      <TailwindProvider utilities={utilities}>
         <View style={styles.container}>
-          <Text>NSBE APP</Text>
+          <Text style={tailwind('text-red-500')}>NSBE APP</Text>
           <StatusBar style="auto" />
         </View>
         <Calendar />
-        
-      </>
+      </TailwindProvider>
+
+
+    </>
   );
 }
 
