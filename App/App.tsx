a1c3c72db35771077b20ca1cf974bf62a496ec96
Shi-morrison <shian.morrison@gmail.com>
2023-12-14 17:14:53 -0500

    attempting tailwind fix

--- App/App.tsx
+++ App/App.tsx
@@ -1,12 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { useTailwind } from 'tailwind-rn';
+import { TailwindProvider } from 'tailwind-rn';
+import utilities from './tailwind.json';
 
 export default function App() {
+  const tailwind = useTailwind();
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <TailwindProvider utilities={utilities}>
+      <View style={styles.container}>
+        <Text style={tailwind('text-red-500')}>Open up App.tsx to start working on your app!!!</Text>
+        <StatusBar style="auto" />
+      </View>
+    </TailwindProvider>
+
   );
 }
 
