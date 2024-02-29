ab2062ef76500c24d9276731d8b310bfdcc191c7
Lewin Bobda <116130662+Lewin-B@users.noreply.github.com>
2024-02-28 23:22:17 -0500

    Added splash screen

--- App/components/Calendar.tsx
+++ App/components/Calendar.tsx
@@ -1,70 +1,68 @@
-import React, { useEffect } from 'react';
-import { View } from 'react-native';
-import { Calendar, Agenda } from 'react-native-calendars';
+import { useEffect } from "react";
+import { View } from "react-native";
+import { Calendar, Agenda } from "react-native-calendars";
 
 interface Event {
-    summary: string;
-    start: {
-      dateTime?: string;
-    };
-    end: {
-      dateTime?: string;
-    };
+  summary: string;
+  start: {
+    dateTime?: string;
+  };
+  end: {
+    dateTime?: string;
+  };
 }
 
 const MyCalendar = () => {
-    const events: Event[] = [];
-    const calendarId = process.env.EXPO_PUBLIC_CALENDAR_ID; //Import NSBE calendar id (Mock Calendar for now)
-    const apiKey = process.env.EXPO_PUBLIC_API_KEY; //Import NSBE Google API Key
-    const apiUrl = `https://www.googleapis.com/calendar/v3/calendars/${calendarId}/events/?key=${apiKey}`; // Merge for api request link
+  const events: Event[] = [];
+  const calendarId = process.env.EXPO_PUBLIC_CALENDAR_ID; //Import NSBE calendar id (Mock Calendar for now)
+  const apiKey = process.env.EXPO_PUBLIC_API_KEY; //Import NSBE Google API Key
+  const apiUrl = `https://www.googleapis.com/calendar/v3/calendars/${calendarId}/events/?key=${apiKey}`; // Merge for api request link
 
+  const fetchData = async () => {
+    try {
+      const response = await fetch(apiUrl); // Make initial api call
+      const data = await response.json(); // Parse the data response
 
-    const fetchData = async () => {
-        try {
-            const response = await fetch(apiUrl); // Make initial api call
-            const data = await response.json(); // Parse the data response
+      if (data.items) {
+        data.items.forEach((event: Event) => {
+          // Store each event in the Events list
+          events.push({
+            summary: event.summary,
+            start: event.start,
+            end: event.end,
+          });
+        });
+      }
+      console.log(events);
+    } catch (error) {
+      console.log("Error fetching data:", error); // log any errors caught
+    }
+  };
 
-            if (data.items) {
-                data.items.forEach((event: Event) => {
-                    // Store each event in the Events list
-                    events.push({
-                        summary: event.summary,
-                        start: event.start,
-                        end: event.end,
-                    });
-                    
-                });
-            }
-            console.log(events)
-        } catch (error) {
-            console.log('Error fetching data:', error); // log any errors caught
-        }
-    };
+  useEffect(() => {
+    fetchData(); // Call function on every new render of the calendar
+  }, []);
 
-    useEffect(() => {
-        fetchData(); // Call function on every new render of the calendar
-    }, []);
-
-    // Create tailwind object
-    return (
-        <View className='flex-1 bg-white'>
-        {/* Get Events fetched from google calendar and display them in the calendar component */}
-        <Calendar
-            current={'2023-01-01'}
-            minDate={'2023-01-01'}
-            maxDate={'2023-12-31'}
-            monthFormat={'yyyy MM'}
-            onDayPress={(day) => {
-            console.log('selected day', day);
-            }}
-            className='bg-blue-100'
-            theme={{
-            arrowColor: 'white',
-            selectedDayBackgroundColor: 'blue-600',
-            selectedDayTextColor: 'white',
-            }}
-        />
-        </View>
+  // Create tailwind object
+  return (
+    <View className="flex-1 bg-white">
+      {/* Get Events fetched from google calendar and display them in the calendar component */}
+      <Calendar
+        current={"2023-01-01"}
+        minDate={"2023-01-01"}
+        maxDate={"2023-12-31"}
+        monthFormat={"yyyy MM"}
+        onDayPress={(day) => {
+          console.log("selected day", day);
+        }}
+        className="bg-blue-100"
+        theme={{
+          arrowColor: "white",
+          selectedDayBackgroundColor: "blue-600",
+          selectedDayTextColor: "white",
+        }}
+      />
+    </View>
   );
 };
 export default MyCalendar;
